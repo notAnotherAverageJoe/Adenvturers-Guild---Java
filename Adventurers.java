8fba6dcdca745ed169ae486e83af9a1f74fedf9d
notAnotherAverageJoe <joeskokan20@.com>
2024-06-16 22:52:55 -0400

    changed the main loop from Adven class to Race class.

--- Adventurers.java
+++ Adventurers.java
@@ -67,6 +67,7 @@ public class Adventurers {
          firstName + " " 
          + lastName +
          " The " + rank+ " ranked" +" "+ classSpec  );
+         
     }
 
 
--- AdventurersGuild.java
+++ AdventurersGuild.java
@@ -9,21 +9,62 @@ public class AdventurersGuild{
         
         System.out.println("How many adventurers are going to be registered: ");
         int numAdv = in.nextInt();
-        Adventurers[] adventurers = new Adventurers[numAdv];
+        Race[] adventurers = new Race[numAdv];
 
         for( int i = 0; i < numAdv; i++){
-            adventurers[i] = new Adventurers();
+            adventurers[i] = new Race();
             adventurers[i].advenRank();
             adventurers[i].advClass();
             adventurers[i].showAdv();
+            adventurers[i].getRaceDetails();
+
             
 
         }
-        // for(Adventurers adven : adventurers) {
-        //     System.out.println(adven);
-        // }
+       // showAdv is from the Adventurer class
         System.out.println(adventurers[0].showAdv());
-        System.out.println(adventurers[1].showAdv());
+        // race details is a child of the adventurer class.
+        System.out.println(adventurers[0].getRaceDetails());
+
+        
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Adventurers[] adventurers = new Adventurers[numAdv];
+
+        // for( int i = 0; i < numAdv; i++){
+        //     adventurers[i] = new Adventurers();
+        //     adventurers[i].advenRank();
+        //     adventurers[i].advClass();
+        //     adventurers[i].showAdv();
+
+            
+
+        // }
+       
+        // System.out.println(adventurers[0].showAdv());
+        // System.out.println(adventurers[1].showAdv());
 
        
         
